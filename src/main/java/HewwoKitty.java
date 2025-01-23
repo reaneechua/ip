3df24425f8ebc5d25f94738bf0a863b66b890825
reaneechua <reaneechua@gmail.com>
2025-01-23 21:45:20 +0800

    uwu wist has been upwoaded

--- src/main/java/HewwoKitty.java
+++ src/main/java/HewwoKitty.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HewwoKitty {
@@ -14,20 +15,36 @@ public class HewwoKitty {
                 "⠀⠀⢀⠴⠚⠻⢧⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠞⠉⠉⠓⠀⠀\n" +
                 "⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠶⠶⠶⣶⣤⣴⡶⠶⠶⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀";
         System.out.println(logo);
+        System.out.println("*************************************************");
         System.out.println("hewwo! i'm hewwo kitty!");
         System.out.println("i'm just a chatbot uwuwuuwuwuwuwuwuw");
+        System.out.println("*************************************************");
 
         boolean run = true;
+        ArrayList<String> items = new ArrayList<>();
         while (run) {
             Scanner userInput = new Scanner(System.in);
             String userString = userInput.nextLine();
-            String exitString = "bye";
-            if (userString.equals(exitString)) {
+            if (userString.equals("bye")) {
                 run = false;
+                System.out.println("*************************************************");
                 System.out.println("goodbwye! uwu");
+                System.out.println("*************************************************");
             } else {
-                System.out.println(userString);
+                if (userString.equals("list")) {
+                    System.out.println("*************************************************");
+                    for (int i = 1; i < items.size() + 1; i++) {
+                        System.out.println(i + ". " + items.get(i-1) + "\n");
+                    }
+                    System.out.println("*************************************************");
+                } else {
+                    System.out.println("*************************************************");
+                    System.out.println("added: " + userString);
+                    System.out.println("*************************************************");
+                    items.add(userString);
+                }
             }
+
         }
     }
 }
